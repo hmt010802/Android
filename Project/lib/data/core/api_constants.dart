115f01a134a8fd86e4916526a5ee67d1510d0cb2
NguyenVanThanh20010798 <thanh131208@gmail.com>
2023-05-21 22:00:55 +0700

    Project Movie App
    Movie App Project cuối kì

---
+++ Project/lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "77918997a50a85e2fb338a9a269107b9";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
